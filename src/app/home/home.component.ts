f6bf3328cfacbadfcba7616cd548098060854b6a
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2021-05-09 19:57:51 -0700

    can write data to firebase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as MediumEditor from 'medium-editor';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { AuthService } from '../services/auth.service';
 
@@ -11,30 +11,39 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  user: Observable<any>;
+  userId: string;
   name: string;
   content: string;
 
   constructor(
     private afAuth: AngularFireAuth,
-    private authService: AuthService
+    private authService: AuthService,
+    private firestore: AngularFirestore // private realTimeDb: AngularFireDatabase,
   ) {}
 
   ngOnInit(): void {
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.userId = user.uid;
+        this.name = user.displayName.split(' ')[0];
+      }
+    });
+
     const editor = new MediumEditor('.editable');
 
     const handleContentChange = () => {
       console.log('get content:', editor.getContent());
       this.content = editor.getContent();
+
+      this.firestore
+        .collection('editor-content')
+        .add({ userId: this.userId, content: this.content })
+        .then(function (res) {
+          console.log(res);
+        });
     };
 
     editor.subscribe('editableInput', handleContentChange.bind(this));
-
-    this.afAuth.authState.subscribe((user) => {
-      if (user) {
-        this.name = user.displayName.split(' ')[0];
-      }
-    });
   }
 
   handleLogout() {
